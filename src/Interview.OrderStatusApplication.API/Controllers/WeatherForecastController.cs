5dcf2de3efc001f314f88991f8f4c7a67fa44a8e
Ellis,Daniel <daniel.ellis@uk.aswatson.com>
2024-02-28 15:22:53 +0000

    Test commit

--- src/Interview.OrderStatusApplication.API/Controllers/WeatherForecastController.cs
+++ src/Interview.OrderStatusApplication.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Interview.OrderStatusApplication.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Muggy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
